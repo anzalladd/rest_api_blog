854e2aa858adb2d35629ad5ef703af35c38d5d45
anzalladde <anzalladd@gmail.com>
2018-10-16 17:32:32 +0700

    read

---
+++ api/post/read_single.php
@@ -0,0 +1,32 @@
+<?php 
+
+	// Header
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+	include_once '../../config/Database.php';
+	include_once '../../models/Post.php';
+
+	// Instantiate DB & Connect
+	$database = new Database();
+	$db = $database->connect();
+	// Instantiate blog post object
+	$post = new Post($db);
+
+	// get id 
+	$post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+	// get post 
+	$post->read_single();
+
+	//  create array
+	$post_arr = array(
+		'id' => $post->id,
+		'title' => $post->title,
+		'body' => $post->body,
+		'author' => $post->author,
+		'category_id' => $post->category_id,
+		'category_name' => $post->category_name
+	);
+
+	// make json
+	print_r(json_encode($post_arr));
